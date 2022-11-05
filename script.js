d7ef7204a3833c2ff4a5d53710f6b58b1428d42f
unknown <vinithkaradi747@gmail.com>
2022-11-05 10:06:35 +0530

    FIx: Responsive design for desktop

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ var check = function() {
     let cnf_psw = document.getElementById('confirm_pswd');
     if (psw.value == cnf_psw.value) {
         document.getElementById('chck').style.color = 'green';
-        document.getElementById('chck').innerHTML = '(Passwords match)';
+        document.getElementById('chck').innerHTML = ' \u2713 Passwords match';
     } else {
         document.getElementById('chck').style.color = 'red';
-        document.getElementById('chck').innerHTML = 'Passwords don\'t match !!!';
+        document.getElementById('chck').innerHTML = ' * Passwords don\'t match !!!';
     }
     
 }
+
+let submitButton = document.getElementById('submit');
+submitButton.addEventListener('click',() => {
+    document.querySelector('form').submit();
+})
