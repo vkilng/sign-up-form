30e1da1952119eacb4dec601e0ea9154e105153e
unknown <vinithkaradi747@gmail.com>
2022-11-05 10:30:44 +0530

    Fix: Submit button outside the form fixed

--- script.js
+++ script.js
@@ -2,16 +2,11 @@ var check = function() {
     let psw = document.getElementById('password');
     let cnf_psw = document.getElementById('confirm_pswd');
     if (psw.value == cnf_psw.value) {
-        document.getElementById('chck').style.color = 'green';
-        document.getElementById('chck').innerHTML = ' \u2713 Passwords match';
+        document.getElementById('check').style.color = 'green';
+        document.getElementById('check').innerHTML = ' \u2713 Passwords match';
     } else {
-        document.getElementById('chck').style.color = 'red';
-        document.getElementById('chck').innerHTML = ' * Passwords don\'t match !!!';
+        document.getElementById('check').style.color = 'red';
+        document.getElementById('check').innerHTML = ' * Passwords don\'t match !!!';
     }
     
 }
-
-let submitButton = document.getElementById('submit');
-submitButton.addEventListener('click',() => {
-    document.querySelector('form').submit();
-})
