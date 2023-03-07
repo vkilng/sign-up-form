b48299d14efcb768da08dd51637f93ecd4a5784e
unknown <vinithkaradi747@gmail.com>
2023-03-07 19:16:08 +0530

    Revamped to make mobile friendly

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 var check = function() {
-    let psw = document.getElementById('password');
-    let cnf_psw = document.getElementById('confirm_pswd');
-    let checkForPassword = document.getElementById('check');
+    const psw = document.getElementById('password');
+    const cnf_psw = document.getElementById('confirm_pswd');
+    const checkForPassword = document.querySelector('.check');
+    const formSubmitButton = document.querySelector('.submit-section button');
     if ((psw.value != cnf_psw.value) && (cnf_psw.value != '')) {
         checkForPassword.style.color = 'red';
         checkForPassword.textContent = ' * Passwords don\'t match !!!';
-        document.querySelector('#submitBtn').disabled = 'disabled';
+        formSubmitButton.setAttribute('disabled','disabled');
     } else if ((psw.value === cnf_psw.value) && (cnf_psw.value != '')) {
         checkForPassword.style.color = 'green';
         checkForPassword.textContent = ' \u2713 Passwords match';
-        document.querySelector('#submitBtn').removeAttribute('disabled');
+        formSubmitButton.removeAttribute('disabled');
     } else {
         checkForPassword.textContent = '';
     }
